9d586c3b76ddf82522e5600a3a86a03a9b6dfa3c
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2023-10-20 11:03:02 +0200

    Update WeatherForecastController.cs

--- signalr-test/Controllers/WeatherForecastController.cs
+++ signalr-test/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
 
 namespace signalr_test.Controllers;
 [ApiController]
@@ -10,11 +11,13 @@ public class WeatherForecastController : ControllerBase
         "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
     };
 
+    private readonly IHubContext<ChatHub> _chatHub;
     private readonly ILogger<WeatherForecastController> _logger;
 
-    public WeatherForecastController(ILogger<WeatherForecastController> logger)
+    public WeatherForecastController(ILogger<WeatherForecastController> logger, IHubContext<ChatHub> chatHub)
     {
         _logger = logger;
+        _chatHub = chatHub;
     }
 
     [HttpGet(Name = "GetWeatherForecast")]
@@ -27,5 +30,15 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
+    }
+
+    [HttpGet("TestGet", Name = "TestGet")]
+    public async Task<IActionResult> TestGet(string type)
+    {
+        if (true)
+        {
+            await _chatHub.Clients.All.SendAsync("testGetReceived", type);
+        }
+        return Ok();
     }
 }
