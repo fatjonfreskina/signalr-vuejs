3047392e6967f33a2e6fd1d48b7ef429b90c0bb2
Fatjon Freskina <91606088+fatjonfreskina@users.noreply.github.com>
2023-10-20 11:02:18 +0200

    Update HelloWorld.vue

--- front-end/signalr-frontend/src/components/HelloWorld.vue
+++ front-end/signalr-frontend/src/components/HelloWorld.vue
@@ -3,13 +3,22 @@
     <h1> Total users: {{ totalUsers }}</h1>
     <button @click="addOne">Add One</button>
     <p>{{ counterButton }}</p>
+    <button @click="sendUpdate">Send update</button>
+    <p>Need update:{{ needsUpdate }}</p>
+    <input type="text" v-model="userMockId" placeholder="User id">
+    <input type="text" v-model="receiverId" placeholder="Receiver id">
+    <p style="white-space: pre-line;">{{ message }}</p>
+<textarea v-model="message" placeholder="Message to send"></textarea>
+<button @click="sendMessage">Send Message to user</button>
+
   </div>
 </template>
 
 <script>
 import * as signalR from '@microsoft/signalr';
+
 const connection = new signalR.HubConnectionBuilder()
-  .withUrl("https://localhost:7158/chatHub")
+  .withUrl(`https://localhost:7158/chatHub/`)
   .withAutomaticReconnect()
   .configureLogging(signalR.LogLevel.Information)
   .build();
@@ -22,12 +31,29 @@ export default {
     return {
       counterButton : 0, 
       totalUsers:0,
+      needsUpdate:false,
+      userMockId: '',
+      receiverId: '',
+      message: ''
     }
   },
 
   created()
   {
     this.start();
+
+    connection.on("testGetReceived", (type) =>{
+      console.log(`Received from get: ${type}`);
+    })
+
+    connection.on("DirectMessageReceived", (user, message) =>{
+      console.log(`Received: ${message} from ${user}`);
+    })
+
+    connection.on("UpdateMenuReceived", (value) => {
+      this.needsUpdate = value;
+      console.log(`Received update: ${value}`);
+    })
 
     connection.on("addOneReceived", (value) => {
       this.counterButton = value;
@@ -62,6 +88,16 @@ export default {
     addOne(){
       this.counterButton++;
       connection.send("AddOne")
+    },
+
+    sendUpdate(){
+      // backend riceve ok
+      console.log("Sending update");
+      connection.send("UpdateMenu");
+    },
+
+    sendMessage() {
+
     }
   },
   
